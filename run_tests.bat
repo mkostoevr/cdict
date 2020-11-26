ae10e35730bd0ad7099fbafdcfc5479b69cf244a
Magomed Kostoev <mkostoevr@yandex.ru>
2020-11-26 22:15:59 +0300

    Refactoring: Update tests

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,6 @@
 tcc example.c -run
 tcc example_custom_allocator.c -run
+tcc example_custom_assert.c -run
 tcc example_custom_comparator.c -run
 tcc example_custom_hash_function.c -run
+tcc example_custom_key_type.c -run
